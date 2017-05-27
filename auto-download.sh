0949d14d76d9086ec628b2aa7d37c95b456606aa
Jason Song <wolfogre@qq.com>
2017-05-27 15:28:30 +0800

    Update auto-download.sh

--- auto-download.sh
+++ auto-download.sh
@@ -1,3 +1,7 @@
-curl -sSL https://github.com/wolfogre/docker-make/archive/v0.3.tar.gz -o /tmp/docker-make.tar.gz && \
-tar xzvf /tmp/docker-make.tar.gz -C . && \
+#!/bin/bash
+
+set -e
+set -x
+curl -sSL https://github.com/wolfogre/docker-make/archive/v0.3.tar.gz -o /tmp/docker-make.tar.gz
+tar xzvf /tmp/docker-make.tar.gz -C .
 mv docker-make-0.3 docker-make
